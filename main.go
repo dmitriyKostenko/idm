0a87a95c108864313dafc7a8558e8855a7f716b8
kostenko.dv <dmitriy.kostenko@sbermarket.ru>
2025-05-29 12:51:27 +0300

    updated go version

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Go.")
+
+	if err := someFunction(); err != nil {
+		fmt.Println("Error occurred")
+
+		return
+	}
+
+	if true {
+		fmt.Println("Hello, World!")
+	}
+}
+
+func someFunction() error {
+	return fmt.Errorf("this is an error")
 }
